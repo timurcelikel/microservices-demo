364be23b9ef4d129f91cbface48b526d0b3897a1
Timur Celikel <timur@procellahealth.com>
2023-07-29 19:34:23 -0700

    Made some changes to pom file and created twitter-to-kafka microservice entry point

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.twitter.to.kafka.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+	}
+
+}
